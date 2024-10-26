ef8a24887e811b4d11d2cfcad2b55d2a3fae04c7
Extraclamation <174183073+Extraclamation@users.noreply.github.com>
2024-10-26 13:52:21 +0200

    Added basic documentation + doctests

--- src/lib.rs
+++ src/lib.rs
@@ -1,2 +1,5 @@
+#![warn(missing_docs)]
+//! A crate containing a basic LIFO stack. Made for practise purposes.
+
 mod stack;
 pub use stack::Stack;
--- src/stack.rs
+++ src/stack.rs
@@ -1,3 +1,10 @@
+/// A struct implementing a basic LIFO stack.
+/// ```
+/// let mut stack: stack::Stack<u8> = stack::Stack::new();
+/// stack.push(1);
+/// stack.push(2);
+/// println!("{:?}, {:?}", stack.pop(), stack.pop());  // 2, 1
+/// ```
 pub struct Stack<T> {
     stack: Vec<T>,
 }
@@ -9,22 +16,54 @@ impl<T> Default for Stack<T> {
 }
 
 impl<T> Stack<T> {
+    /// Create a new empty stack.
+    /// ```
+    /// let mut stack: stack::Stack<u8> = stack::Stack::new();
+    /// ```
     pub fn new() -> Self {
         Default::default()
     }
 
+    /// Pushes an item to the end of stack.
+    /// ```
+    /// # let mut stack: stack::Stack<u8> = stack::Stack::new();
+    /// println!("{:?}", stack.push(1)); // 1
+    /// println!("{:?}", stack.push(2)); // 1, 2
+    /// ```
     pub fn push(&mut self, item: T) {
         self.stack.push(item);
     }
 
+    /// Pops an item from the end of the stack. Returns None if the stack is empty.
+    /// ```
+    /// # let mut stack: stack::Stack<u8> = stack::Stack::new();
+    /// stack.push(1);
+    /// println!("{:?}", stack.pop()); // Some(1)
+    /// println!("{:?}", stack.pop()); // None
+    /// ```
     pub fn pop(&mut self) -> Option<T> {
         self.stack.pop()
     }
 
+    /// Returns the item at the end of the stack without removing it. Returns None if the stack is empty.
+    /// ```
+    /// # let mut stack: stack::Stack<u8> = stack::Stack::new();
+    /// stack.push(1);
+    /// println!("{:?}", stack.peek()); // Some(1)
+    /// println!("{:?}", stack.pop()); // Some(1)
+    /// ```
     pub fn peek(&self) -> Option<&T> {
-        self.stack.first()
+        self.stack.last()
     }
 
+    /// Returns true if the stack is empty.
+    /// ```
+    /// # let mut stack: stack::Stack<u8> = stack::Stack::new();
+    /// stack.push(1);
+    /// println!("{:?}", stack.is_empty()); // false
+    /// stack.pop();
+    /// println!("{:?}", stack.is_empty()); // true   
+    /// ```
     pub fn is_empty(&self) -> bool {
         self.stack.is_empty()
     }
